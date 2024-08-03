2512f66b47b106288fc86dac349be4195d8dffd6
alexander apraez <apraez.consultant@gmail.com>
2024-08-03 11:53:22 -0500

    Aplicación CAFENAR Login, Register, G productos

---
+++ carritoBasicAvanzado-main/src/components/Register.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    email: ''
+  });
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/auth/register', formData);
+      setMessage('Registro exitoso');
+      localStorage.setItem('username', formData.username);
+      window.dispatchEvent(new Event('storage')); // Disparar evento de almacenamiento
+      navigate('/');
+    } catch (error) {
+      setMessage(error.response?.data?.error || 'Error en el registro');
+    }
+  };
+
+  return (
+    <div className="payment-form">
+      <form onSubmit={handleSubmit}>
+        <h2>Registro</h2>
+        {message && <p>{message}</p>}
+        <label>
+          Nombre de Usuario:
+          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <label>
+          Contraseña:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <button type="submit">Registro</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
